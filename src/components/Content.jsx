c28f46d668b63ae0f5e8688cae60f2adb94dd4a0
neoxxx13 <www.neoxxx.com@gmail.com>
2023-02-17 12:06:51 +0200

    new project

---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+function Content () {
+    return(
+        <div className="content">
+            <div className="fon">
+                <img src="https://images.unian.net/photos/2022_06/1656498364-2927.jpg?r=274843" alt="фото" />
+            </div>
+            <h1 className="zag">заголовок</h1>
+            <div className="text">text</div>
+        </div>
+    );
+}
+
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header () {
+    return(
+        <header className='header'>
+        <img src="https://v.od.ua/uploads/92/logo.png" alt="лого" />
+        </header>
+    );
+}
+
+export default Header;
